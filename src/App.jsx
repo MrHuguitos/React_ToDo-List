a3d9b9ae0f849b51fa84ec1d3bc7bd57fc88aa17
MrHuguitos <155857650+MrHuguitos@users.noreply.github.com>
2026-07-24 10:23:34 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import './App.css'
 import Login from './components/Login.jsx'
 import Dashboard from './components/Dashboard.jsx'
 
